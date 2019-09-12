170f063a08ddcfb8009be0189da7325b5e1a797b
Daisy Alary <daisy.alary@exigis.com>
2019-09-12 14:17:21 -0700

    heroku redis setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.web_socket_server_url = "wss://message-me-daisy.herokuapp.com/cable"
 end
